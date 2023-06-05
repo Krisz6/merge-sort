0ced468ded3906115c1dbf81d68914b8ee08aec9
Krisz6 <szalay.krisztian93@gmail.com>
2023-06-05 19:41:28 +0200

    add merge method

--- src/Main.java
+++ src/Main.java
@@ -4,4 +4,7 @@ public class Main {
     public static boolean isSorted(int[] array){
         return false;
     }
+    public static int[] merge(int[] left, int[] right){
+        return null;
+    }
 }
